6d3d7f634cf99be69a60f08e9fa342d5c674bdbe
Salputrif <90301030+Salputrif@users.noreply.github.com>
2021-10-28 11:50:51 +0700

    Update UTS.py

--- UTS.py
+++ UTS.py
@@ -1,3 +1,75 @@
+class Uang:
+    def __init__(self,saldo_umum, saldo_tabungan,tambah_umum,tambah_tabungan, ambil_umum,ambil_tabungan):
+        self.saldo=saldo_umum
+        self.saldo1=saldo_tabungan
+        self.tambah=tambah_umum
+        self.tambah1=tambah_tabungan
+        self.ambil=ambil_umum
+        self.ambil1=ambil_tabungan
+
+    def saldo_umum(self):
+        print("Saldo umum Anda saat ini adalah: Rp.0,-", self.saldo)
+
+    def saldo_tabungan(self):
+        print("Saldo tabungan Anda saat ini adalah: Rp.0,-", self.saldo1)
+
+    def tambah_umum(self):
+        print("Nominal uang yang akan ditambahkan :", self.tambah)
+
+    def tambah_tabungan(self):
+        print("Nominal uang yang akan ditambahkan :", self.tambah1)
+
+    def ambil_umum(self):
+        print("Nominal Uang yang akan diambil :", self.ambil)
+    
+    def ambil_tabungan(self):
+        print("Nominal Uang yang akan diambil :", self.ambil1)
+
+
+#Kelas Turunan
+
+class InformasiUm(Uang):
+    def Infoumum(self):
+        Uang.saldo_umum(self)
+
+    def keterangan(self):
+        print("Saldo umum Anda saat ini adalah: Rp.0,-")
+
+class InformasiTab(Uang):
+    def InfoTabungan(self):
+        Uang.saldo_tabungan(self)
+
+    def keterangan(self):
+        print("Saldo Tabungan Anda saat Ini adalah: Rp.0,-")
+
+class TambahUm(Uang):
+    def TambahUmum(self):
+        Uang.tambah_umum(self)
+
+    def keterangan(self):
+        print("Nominal uang yang akan ditambahkan :")
+
+class TambahTab(Uang):
+    def TambahTabungan(self):
+        Uang.tambah_tabungan(self)
+
+    def keterangan(self):
+        print("Nominal Uang yang akan Ditambahkan :")
+
+class AmbilUm(Uang):
+    def AmbilUmum(self):
+        Uang.ambil_umum(self)
+
+    def keterangan(self):
+        print("Nominal Uang yang akan diambil :")
+
+class AmbilTab(Uang):
+    def AmbilTabungan(self):
+        Uang.ambil_tabungan(self)      
+
+    def keterangan(self):
+        print("Nominal Uang yang akan diambil :")  
+
 print("***Aplikasi Pencatatan Uang Digital***")
 print("**************************************")
 print("[1] Informasi Saldo")
@@ -6,21 +78,39 @@ print("[3] Ambil Saldo")
 print("[4] Keluar")
 print("**************************************")
 option=int(input("PIlih Menu:"))
-uang=0
 if option==1:
-    print("Saldo umum Anda saat ini adalah: Rp.0,-")
-    print("Saldo tabungan Anda saat ini adalah: Rp.0,-")
-    print("**************************************")
+    i=InformasiUm
+    i.Infoumum
+    i.keterangan
+
+    i=InformasiTab
+    i.InfoTabungan
+    i.keterangan
+
 elif option==2:
     print("[1] Umum")
     print("[2] Tabungan")
-    option2=int(input("Pilih Penyimpanan:"))
-    print("**************************************")
-    if option2==1:
-    option3=int(input("Nominal uang yang akan ditambahkan :"))
-    hasil1=uang+option3
-    print("Saldo umum Anda saat ini adalah:",hasil1)
-    elif option2==2:
-    option4=int(input("Nominal uang yang akan ditambahkan :"))
-    hasil2=uang+option4
-    print("Saldo umum Anda saat ini adalah:",hasil2)
+    option=int(input("Pilih Penyimpanan:"))
+    print("**************")
+if option==1:
+    i=TambahUm
+    i.TambahUmum()
+    i.keterangan()
+elif option==2:
+    i=TambahTab
+    i.TambahTabungan
+    i.keterangan
+
+elif option==3:
+    print("[1] Umum")
+    print("[2] Tabungan")
+    option=int(input("Pilih Penyimpanan:"))
+    print("**************")
+if option==1:
+    i=AmbilUm
+    i.AmbilUmum
+    i.keterangan
+elif option==2:
+    i=AmbilTab
+    i.AmbilTabungan
+    i.keterangan
